a9debeb62f5f34191f67b21bfadcfbb71ad2b876
Evian-dev <evian.rebic@gmail.com>
2026-01-22 09:29:48 +0100

    Add files via upload

---
+++ knjiznica/knjiznica/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace knjiznica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Spremi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.textBoxbrojTelefona = new System.Windows.Forms.TextBox();
+            this.ž = new System.Windows.Forms.TextBox();
+            this.textBoxOIB = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Spremi
+            // 
+            this.Spremi.Location = new System.Drawing.Point(317, 327);
+            this.Spremi.Name = "Spremi";
+            this.Spremi.Size = new System.Drawing.Size(75, 23);
+            this.Spremi.TabIndex = 0;
+            this.Spremi.Text = "Spremi";
+            this.Spremi.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prezime:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "E-mail:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Broj telefona:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Adresa korisnika:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(218, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "OIB:";
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(354, 108);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIme.TabIndex = 7;
+            // 
+            // textBoxbrojTelefona
+            // 
+            this.textBoxbrojTelefona.Location = new System.Drawing.Point(354, 196);
+            this.textBoxbrojTelefona.Name = "textBoxbrojTelefona";
+            this.textBoxbrojTelefona.Size = new System.Drawing.Size(100, 20);
+            this.textBoxbrojTelefona.TabIndex = 8;
+            // 
+            // ž
+            // 
+            this.ž.Location = new System.Drawing.Point(354, 222);
+            this.ž.Name = "ž";
+            this.ž.Size = new System.Drawing.Size(100, 20);
+            this.ž.TabIndex = 9;
+            // 
+            // textBoxOIB
+            // 
+            this.textBoxOIB.Location = new System.Drawing.Point(354, 250);
+            this.textBoxOIB.Name = "textBoxOIB";
+            this.textBoxOIB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOIB.TabIndex = 10;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(354, 168);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmail.TabIndex = 11;
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(354, 140);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrezime.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxOIB);
+            this.Controls.Add(this.ž);
+            this.Controls.Add(this.textBoxbrojTelefona);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Spremi);
+            this.Name = "Form1";
+            this.Text = "Unos korisnika";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Spremi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.TextBox textBoxbrojTelefona;
+        private System.Windows.Forms.TextBox ž;
+        private System.Windows.Forms.TextBox textBoxOIB;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+    }
+}
+
